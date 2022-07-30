7d39746fddc390c6908cfadb47ca497272ae87bf
Nguyễn Văn Đạt <nguyendat06611122000@gmail.com>
2022-07-31 01:17:39 +0700

    Ex6 8 9.

---
+++ Ex6b.java
@@ -0,0 +1,16 @@
+public class Ex6b{
+    public static void main(String[] args){
+        System.out.println(fibonacci(2));
+    }
+    public static int fibonacci(int n){
+        if (n == 0){
+            return 0;
+        } 
+        else if (n == 1){
+            return 1;
+        }
+        else{
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+}
