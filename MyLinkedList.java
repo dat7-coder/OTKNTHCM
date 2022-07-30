aaca90d9b959257d2748528a667cbe6d6a89edd5
nghiadang-vizion <nghia.dang@vizion.space>
2022-07-30 19:51:25 +0700

    Nghia commit

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -3,7 +3,7 @@ public class MyLinkedList{
     private int numNode;
     public MyLinkedList(){
         this.head = null;
-        this.numNode = 0;
+        this.numNode = 1;
     }
     public boolean addLast(int item){
         Node newNode = new Node(item);
@@ -11,22 +11,119 @@ public class MyLinkedList{
             head = newNode;
         }else{
             Node last = head;
-            while(last.next != null){
-                if (newNode == last){
+            while(last.getNext() != null){
+                if (newNode.getData() == last.getData()){
                     return false;
                 }else{
-                    last = last.next;
+                    last = last.getNext();
                 }
             }
-            last.next = newNode;
+            last.setNext( newNode);
+            numNode=numNode+1;
         }
         return true;
+    }
+    public boolean addFirst(int item){
+        Node newNode = new Node(item);
+        if (head == null){
+            head = newNode;
+        }else{
+            if (newNode.getData() < head.getData()) {
+                return false;
+            }
+            newNode.setNext(head);
+            head = newNode;
+            numNode+=1;
+        }
+        return true;
+    }
+    public boolean addAfter(int item, int k){
+        Node newNode = new Node(item);
+        Node last = head;
+        while(last.getNext() != null){
+            if (k == last.getData()){
+                newNode.setNext(last.next);
+                last.next = newNode;
+                numNode=numNode+1;
+                return true;
+            }else{
+                last = last.getNext();
+            }
+        }
+        return false;
+    }
+    public int removeFirst(){
+        Node temp = head;
+        if (head == null) {
+            return 0;
+        }
+        else{
+            head = temp.next;
+            numNode=numNode-1;
+        }
+        return 1;
+    }
+    public int removeLast(){
+        if (head == null) {
+            return 0;
+        }
+        else{
+            Node last = head;
+            while(last.getNext().getNext() != null){
+                last = last.getNext();
+            }
+            last.setNext(null);
+            
+            numNode=numNode-1;
+        }
+        return 1;
+    }
+    public boolean removeElement(int k){
+        Node last = head;
+        while(last.getNext() != null){
+            if(k == last.getData()){
+                this.removeFirst();
+                numNode=numNode-1;
+                return true;
+            }
+            if (k == last.getNext().getData()){
+                last.setNext(last.getNext().getNext());
+                numNode=numNode-1;
+                return true;
+            }else{
+                last = last.getNext();
+            }
+        }
+        return false;
+    }
+    public int size(){
+        return numNode;
+    }
+    public boolean contains(int item){
+        Node last = head;
+        while(last.getNext() != null){
+            if (item == last.getData()){
+                return true;
+            }else{
+                last = last.getNext();
+            }
+        }
+        return false;
+    }
+    public boolean isEmpty(){
+        if (head == null) {
+            return true;
+        }
+        return false;
+    }
+    public Node getHead(){
+        return head;
     }
     public void print(){
 		Node p = this.head;
 		do{
 			System.out.print(p.data);
-			p=p.next;
+			p=p.getNext();
 		}while ( p != null);
 	}
 }
--- Test.java
+++ Test.java
@@ -1,10 +1,22 @@
 public class Test{
     public static void main(String[] args){
         MyLinkedList ln = new MyLinkedList();
+        
+        System.out.println(ln.removeFirst());
         ln.addLast(1);
         ln.addLast(2);
         ln.addLast(4);
         ln.addLast(5);
+        System.out.println(ln.addAfter(2,4));
         System.out.println(ln.addLast(6));
+        System.out.println(ln.addFirst(6));
+        System.out.println(ln.removeLast());
+        System.out.println(ln.removeElement(2));
+        System.out.println(ln.size());
+        System.out.println(ln.contains(4));
+        System.out.println(ln.isEmpty());
+        System.out.println(ln.getHead());
+        
+        ln.print();
     }
 }
